2945884805eae4493d3babe99a4b564ab8d97d0c
karolina.skiba <karolina.skiba@nxsa.onmicrosoft.com>
2022-01-21 13:29:18 +0100

    dodanie mudule students

---
+++ app/src/store/modules/students/index.ts
@@ -0,0 +1,39 @@
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
+import StudentModel from "@/models/StudentModel";
+
+export default {
+  namespace: true,
+  state() {
+    return {
+      students: <StudentModel[]>[
+        {
+          firstName: "Karolina",
+          lastName: "Skiba",
+          schoolType: 1,
+          schoolName: "v",
+          grade: 1,
+          book: "Pazdro",
+          extension: true,
+          communicator: 1,
+          id: 0,
+        },
+        {
+          firstName: "Karo",
+          lastName: "Skiba",
+          schoolType: 2,
+          schoolName: "vi",
+          grade: 1,
+          book: "Oficyna",
+          extension: false,
+          communicator: 2,
+          id: 2,
+        },
+      ],
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
